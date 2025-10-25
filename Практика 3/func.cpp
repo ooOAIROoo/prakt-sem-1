e8f21569a6bc28be2f786b07cfbe698ba4356cbc
Георгий Бондарев <georgiy.bondarev@gmail.com>
2025-10-25 22:53:17 +0300

    задание 3 , не работает вывод русских букв

--- Практика 3/func.cpp
+++ Практика 3/func.cpp
@@ -3,16 +3,18 @@
 #include <fstream>
 #include <string>
 #include <Windows.h>
+#include <algorithm>
 
 using namespace std;
 
 // C:\test_files\course_1\pr_3\num1.txt
 // C:\test_files\course_1\pr_3\num2.txt
+// C:\test_files\course_1\pr_3\num3.txt
 
 int ex1() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
-    cout << u8"\t ==== Задача 1 ====\n";
+    cout << u8"\t ==== Задача 1 =====\n";
     cout << u8"«Копирование файла». Создать на диске текстовый файл и скопировать его на экран." << endl;
 
     string filename;
@@ -41,7 +43,7 @@ int ex1() {
 int ex2() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
-    cout << u8"\t ==== Задача 2 ===" << endl;
+    cout << u8"\t ==== Задача 2 ====" << endl;
     cout << u8"«Фильтр». Вывести на экран только числа из созданного Вами на диске текстового файла, содержащего буквы и числа." << endl;
 
     wstring filename;
@@ -73,16 +75,38 @@ int ex2() {
 }
 
 int ex3() {
-    cout << "\t ==== Задача 3 ===" << endl;
-    cout << "«Сортировка букв». Задать строку из 30 букв и расставить их в алфавитном порядке.";
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
+    cout << u8"\t ==== Задача 3 ====" << endl;
+    cout << u8"«Сортировка букв». Задать строку из 30 букв и расставить их в алфавитном порядке." << endl;
 
+    wstring filename;
 
+    cout << u8"Укажите путь к файлу: ";
+    wcin >> filename;
+
+    ifstream file_test_3(filename);
+
+    if (!file_test_3.is_open()) {
+        cout << u8"Ошибка открытия файла!" << endl;
+        return 1;
+    }
+
+    string line;
+    while (getline(file_test_3, line)) {
+        sort(line.begin(), line.end());
+        cout << line;
+
+    }
+
+    file_test_3.close();
+    return 0;
 
     return 0;
 }
 
 int ex4() {
-    cout << "\t ==== Задача 4 ===" << endl;
+    cout << "\t ==== Задача 4 ====" << endl;
     cout << "«Алгоритм Евклида». Задать 2 числа и найти их наибольший общий делитель двумя способами : делением и вычитанием.";
 
 
@@ -90,7 +114,7 @@ int ex4() {
 }
 
 int ex5() {
-    cout << "\t ==== Задача 5 ===" << endl;
+    cout << "\t ==== Задача 5 ====" << endl;
     cout << "«Решето Эратосфена». Найти все простые числа в диапазоне от 2 до введенного вами натурального числа.";
 
 
@@ -98,7 +122,7 @@ int ex5() {
 }
 
 int ex6() {
-    cout << "\t ==== Задача 6 ===" << endl;
+    cout << "\t ==== Задача 6 ====" << endl;
     cout << "«Файл». Создать файл, записать в него 10 чисел, закрыть, потом вновь открыть файл и найти сумму чисел.";
 
 
